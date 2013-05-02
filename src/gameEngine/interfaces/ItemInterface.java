d302bc3ffac5b2769cd707a4ab8690bc33599ec3
derfalx <cyberfreakwar@web.de>
2013-05-02 19:16:01 +0200

    Standart Kommentarblock hinzugefügt

--- src/gameEngine/interfaces/ItemInterface.java
+++ src/gameEngine/interfaces/ItemInterface.java
@@ -1,3 +1,19 @@
+/*
+ * ************** WICHTIG: **************************************************************************************
+ *    - Bitte jegliche Änderungen erstmal auskommentieren! Bis zu einem erneutem Update.
+ * 		D.h. es sollen immer die Kommentare zu den letzten Änderungen dabei sein!
+ *    - JavaDoc Kommentare sind erwünscht
+ *    - Bitte dort wo es Sinnvoll ist Kommentare setzten und mit anderen die Änderungen abzusprechen
+ *      um so ein Chaos zu vermeiden!
+ *    - Variablen und Klassen sollten Sinnvoll benannt werden, selbiges gilt für Packages.
+ *    - Diese (http://www.oracle.com/technetwork/java/codeconventions-135099.html) Konventionen sind einzuhalten!
+ *    - Wenn ihr eine neue Datei erstellt (Class, Interface) dann fügt diesen Kommentarblock hinzu, damit überall 
+ *      einheitlich gearbeitet wird!
+ *    
+ *  Danke!
+ * ***************************************************************************************************************
+ */
+
 package gameEngine.interfaces;
 
 public interface ItemInterface 
