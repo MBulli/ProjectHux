b8a81f8e4b75fc728e775f0041179717c494c0db
derfalx <cyberfreakwar@web.de>
2013-05-02 23:46:38 +0300

    Update PlayerInterface.java

--- src/gameEngine/interfaces/PlayerInterface.java
+++ src/gameEngine/interfaces/PlayerInterface.java
@@ -1,13 +1,13 @@
 /*
  * ************** WICHTIG: **************************************************************************************
- *    - Bitte jegliche Änderungen erstmal auskommentieren! Bis zu einem erneutem Update.
- * 		D.h. es sollen immer die Kommentare zu den letzten Änderungen dabei sein!
- *    - JavaDoc Kommentare sind erwünscht.
- *    - Bitte dort wo es Sinnvoll ist Kommentare setzten und mit anderen die Änderungen abzusprechen
+ *    - Bitte jegliche ï¿½nderungen erstmal auskommentieren! Bis zu einem erneutem Update.
+ * 		D.h. es sollen immer die Kommentare zu den letzten ï¿½nderungen dabei sein!
+ *    - JavaDoc Kommentare sind erwï¿½nscht.
+ *    - Bitte dort wo es Sinnvoll ist Kommentare setzten und mit anderen die ï¿½nderungen abzsprechen
  *      um so ein Chaos zu vermeiden!
- *    - Variablen und Klassen sollten Sinnvoll benannt werden, selbiges gilt für Packages.
+ *    - Variablen und Klassen sollten Sinnvoll benannt werden, selbiges gilt fï¿½r Packages.
  *    - Diese (http://www.oracle.com/technetwork/java/codeconventions-135099.html) Konventionen sind einzuhalten!
- *    - Wenn ihr eine neue Datei erstellt (Class, Interface) dann fügt diesen Kommentarblock hinzu, damit überall 
+ *    - Wenn ihr eine neue Datei erstellt (Class, Interface) dann fï¿½gt diesen Kommentarblock hinzu, damit ï¿½berall 
  *      einheitlich gearbeitet wird!
  *    
  *  Danke!
