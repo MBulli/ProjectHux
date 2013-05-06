0885a742f01261618c9086c355d176567ce48ddf
MBulli <markusbullmann@gmail.com>
2013-05-06 13:53:35 +0200

    Eclipse Projekt- und Ordnerstruktur angelegt

---
+++ Code/Project Hux/src/de/hux/hux/Main.java
@@ -0,0 +1,15 @@
+package de.hux.hux;
+
+import de.hux.hux.graphics.*;
+
+public final class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JOGL2Setup_GLCanvas.joglTest();
+	}
+
+}
